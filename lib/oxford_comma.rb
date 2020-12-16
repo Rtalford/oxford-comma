ab26397e4d627a464f566144f53af512bacfc829
Reynelle <Rtalford13@gmail.com>
2020-12-16 11:11:07 -0500

    commit tree 580312a80188abf2a1f4509f7b632d7105eddf85

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
-def oxford_comma(array)
 
-end
+def oxford_comma(array)
+    if array.count == 1
+    array.join
+    elsif array.count == 2
+       array.join (' and ')
+    else
+   last_element = array.pop
+   array.join(", ") << ", and #{last_element}"
+
+   end 
+end 
+
+
+   
